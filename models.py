e539ff4d38afe424de326f6d644389d58a9f3a97
siddhartha borpuzari <53326014+siddhartha4u2c@users.noreply.github.com>
2026-02-07 19:44:04 +0530

    Update models.py

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import db
+from extensions import db  
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'

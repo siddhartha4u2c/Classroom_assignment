be3b99238354141fbec6957cae29566977af3f99
siddhartha borpuzari <53326014+siddhartha4u2c@users.noreply.github.com>
2026-02-07 19:06:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ def _database_uri():
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = "postgresql://classroom_yvom_user:9ZUY5CGlKnXfGE7TIx1SqT9D0d7XmRVs@dpg-d63j22ngi27c739h3qag-a.postgres-render.com:5432/classroom_yvom"
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg://classroom_yvom_user:9ZUY5CGlKnXfGE7TIx1SqT9D0d7XmRVs@dpg-d63j22ngi27c739h3qag-a.postgres-render.com:5432/classroom_yvom"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16 MB max file size
